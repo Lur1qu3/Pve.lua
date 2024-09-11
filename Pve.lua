3515cd54e109c36540d5d75183717241ceafb3b4
Lur1qu3 <mcmclhmm1@gmail.com>
2024-09-11 00:05:43 -0300

    Update Pve.lua

--- Pve.lua
+++ Pve.lua
@@ -141,6 +141,62 @@ UI.Separator()
 
 
 
+local castBelowHp = 70 
+
+mystic60 = macro(100, "Mystic Defense/Kai",  function()
+  if (hppercent() < castBelowHp and not hasManaShield()) then
+    say('Mystic Defense') 
+  end
+  if (hppercent() >= castBelowHp and hasManaShield()) then
+    say('Mystic Kai')
+  end
+end,hpPanel5)
+
+
+
+local panelName = "codPan"
+local codPanel = setupUI([[
+Panel
+  id: healingPanel
+  height: 50
+  margin-top: 3
+
+  Label
+    id: label2
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: parent.top
+    margin: 0 5 0 5
+    text-align: center
+
+  HorizontalScrollBar
+    id: scroll1
+    anchors.left: parent.left
+    anchors.right: parent.right
+    anchors.top: prev.bottom
+    margin-top: 5
+    minimum: 1
+    maximum: 100
+    step: 1
+
+]])
+
+if not storage[panelName] then
+    storage[panelName] = {
+        hpPercent = castBelowHp 
+    }
+end
+    
+codPanel.scroll1.onValueChange = function(scroll, value)
+  castBelowHp = value 
+  storage[panelName].hpPercent = value 
+  codPanel.label2:setText("MYSTIC%" .. castBelowHp)
+end
+
+
+codPanel.scroll1:setValue(storage[panelName].hpPercent or castBelowHp)
+
+addIcon("Def/kai", {item=672, movable=true, text = "Def/kai"}, mystic60)
 
 
 
